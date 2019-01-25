971fc0229f637c7bafb0609b5b42170072c07f86
Wenceslao Llobet <wenceslao.llobet.sge@mango.com>
2019-01-25 16:50:25 +0100

    iteration 4 done

--- src/test/java/ApplicationFeature.java
+++ src/test/java/ApplicationFeature.java
@@ -1,3 +1,4 @@
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,7 +9,21 @@ public class ApplicationFeature {
     private LeanApplication application = new LeanApplication();
 
     @Test
-    public void beTesteable() {
-        assertThat(application.process("apples"),is("100"));
+    public void iteration_3() {
+
+        assertThat(application.processCSV("apples,cherries,bananas"),is(325));
+        application = new LeanApplication();
+        assertThat(application.processCSV("cherries,cherries"),is(130));
+    }
+
+
+    @Test
+    public void iteration_4() {
+        assertThat(application.process("cherries"),is(75));
+        assertThat(application.process("pommes"),is(175));
+        assertThat(application.process("cherries"),is(230));
+        assertThat(application.process("bananas"),is(380));
+        assertThat(application.process("bananas"),is(380));
+        assertThat(application.process("apples"),is(480));
     }
 }
--- src/test/java/LeanApplication.java
+++ src/test/java/LeanApplication.java
@@ -1,5 +1,64 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class LeanApplication {
-    public String process(String apples) {
-        return "100";
+    private int subtotal = 0;
+    private int numberOfCherries = 0;
+    private int numberOfBannanas = 0;
+    private HashSet apples = new HashSet() {{
+        add("meles");
+        add("pommes");
+        add("apples");
+    }};
+
+    public int process(String fruitName) {
+        return calculateSubtotal(fruitName) - calculateDiscounts();
+    }
+
+    private int calculateSubtotal(String fruitName) {
+        if (fruitName.equals("cherries")) {
+            subtotal += 75;
+            numberOfCherries++;
+        }
+
+        if (isApple(fruitName)) {
+            subtotal += 100;
+        }
+
+        if (fruitName.equals("bananas")) {
+            subtotal += 150;
+            numberOfBannanas++;
+        }
+        return subtotal;
+    }
+
+    private boolean isApple(String fruitName) {
+        return apples.contains(fruitName);
+    }
+
+    private int calculateDiscounts() {
+        return (int)(
+                (Math.floor(numberOfCherries / 2)) * 20 +
+                (Math.floor(numberOfBannanas / 2)) * 150
+        );
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LeanApplication app = new LeanApplication();
+        String input = "";
+        while (!"exit".equals(input)) {
+            input = scanner.next();
+            System.out.println(input + " => " + app.process(input));
+        }
+    }
+
+    public int processCSV(String csv) {
+        String[] fruits = csv.split(",");
+        int restul = 0;
+        for (String fruit : fruits) {
+            restul = process(fruit);
+        }
+        return restul;
     }
 }
